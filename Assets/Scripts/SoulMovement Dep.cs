375fffb13b8de01130a54a615e3c9e549f4d7ebd
Marlus Vinicius <marlusvinicius2014@outlook.com>
2020-12-29 10:09:56 -0300

    Initial commit :)

---
+++ Assets/Scripts/SoulMovement Dep.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public enum JUMPSTATE
+{
+    ONGROUND,
+    RISING,
+    FALLING
+};
+public class SoulMovementDep : MonoBehaviour
+{
+    private Collider2D _collider;
+    private float jumpMaxHeight; 
+    private float soulHeight;
+    private JUMPSTATE _jumpstate;
+    
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float jumpUpRate = 2f;
+    [SerializeField] private float jumpDownRate = 2f;
+    [SerializeField] private int maxBodiesJump = 2; //The maximum jump distance is defined by this times the sprite's height;
+    [SerializeField] private int numberOfJumps = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<CapsuleCollider2D>();
+        soulHeight = _collider.bounds.size.y;
+        _jumpstate = JUMPSTATE.FALLING;
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(speed * moveHorizontal,0) * Time.deltaTime, Space.World);
+        Jump();
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.tag.ToLower().Contains("ground"))
+        {
+            numberOfJumps = 1;
+        }
+    }
+
+    private void Jump()
+    {
+        if (numberOfJumps > 0)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                jumpMaxHeight = soulHeight * maxBodiesJump + transform.position.y;
+                _jumpstate = JUMPSTATE.RISING;
+                numberOfJumps--;
+            }
+        }
+
+
+        if (_jumpstate == JUMPSTATE.RISING)
+        {
+            if (transform.position.y < jumpMaxHeight)
+            {
+                transform.Translate(new Vector2(0,jumpUpRate) * Time.deltaTime);
+            }
+
+            else
+            {
+                _jumpstate = JUMPSTATE.FALLING;
+            }
+        }
+        
+        if (_jumpstate == JUMPSTATE.FALLING)
+        {
+            transform.Translate(new Vector2(0,-jumpDownRate) * Time.deltaTime);
+        }
+        
+        
+    }
+    
+    
+}
+
+
---
+++ Assets/Scripts/SoulMovement.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+
+public class SoulMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float jumpForce = 2f;
+    //[SerializeField] float intervalTimer = 1.5f;
+    [SerializeField] private int numberOfJumps = 1;
+
+    private Rigidbody2D rb;
+    //private bool isCountingTime;
+    //private float stockTimer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //stockTimer = intervalTimer;
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(speed * moveHorizontal,0) * Time.deltaTime, Space.World);
+        Jump();
+        //jumpTimer();
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.tag.ToLower().Contains("ground"))
+        {
+            numberOfJumps = 1;
+        }
+    }
+
+    private void Jump()
+    {
+        if (numberOfJumps > 0 /*&& !isCountingTime*/)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                rb.AddForce(new Vector2(0, jumpForce) * Time.deltaTime, ForceMode2D.Impulse);
+                numberOfJumps--;
+                //isCountingTime = true;
+            }
+        }
+        
+        
+    }
+
+    // private void jumpTimer()
+    // {
+    //     if (isCountingTime)
+    //     {
+    //         intervalTimer -= Time.deltaTime;
+    //         if (intervalTimer <= 0)
+    //         {
+    //             isCountingTime = false;
+    //             intervalTimer = stockTimer;
+    //         }
+    //     }
+    // }
+    
+    
+    
+    
+}
